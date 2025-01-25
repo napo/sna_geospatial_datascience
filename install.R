6248d03495d93a080c77bd5147a1ecdd24b582af
Maurizio Napolitano <napo@fbk.eu>
2025-01-26 00:22:56 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,18 +1,8 @@
+# Imposta il repository CRAN
 options(repos = c(CRAN = "https://cloud.r-project.org"))
-# Installa i pacchetti necessari
-install.packages(c("devtools", "IRkernel", "shiny"))
-# Configura IRkernel per Jupyter
-IRkernel::installspec(user = FALSE)
 
-if (!requireNamespace("sf", quietly = TRUE)) {
-  install.packages("sf", repos = "https://cloud.r-project.org/")
-}
-if (!requireNamespace("tidyverse", quietly = TRUE)) {
-  install.packages("tidyverse", repos = "https://cloud.r-project.org/")
-}
-if (!requireNamespace("mapview", quietly = TRUE)) {
-  install.packages("mapview", repos = "https://cloud.r-project.org/")
-}
-if (!requireNamespace("readr", quietly = TRUE)) {
-  install.packages("readr", repos = "https://cloud.r-project.org/")
-}
+# Installa i pacchetti richiesti
+install.packages(c("devtools", "IRkernel", "shiny"))
+
+# Configura il kernel R per Jupyter
+IRkernel::installspec(user = FALSE)
