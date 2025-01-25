1ee652d57be13c229e91e42d276dffa5d1cb855d
Maurizio Napolitano <napo@fbk.eu>
2025-01-26 00:48:01 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,8 +1,7 @@
 # Imposta il repository CRAN
 options(repos = c(CRAN = "https://cloud.r-project.org"))
-
 # Installa i pacchetti richiesti
-install.packages(c("devtools", "IRkernel", "shiny"))
+install.packages(c("devtools", "IRkernel", "shiny","r-cran-irkernel","sf"))
 
 # Configura il kernel R per Jupyter
 IRkernel::installspec(user = FALSE)
